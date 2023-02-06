4c1d189a6a96bc324dd639d9aecc8657b720fd01
Joshua Unger <jhunger@wpi.edu>
2023-02-05 19:56:52 -0500

    began fs work

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,38 @@
+import java.io.BufferedWriter;
+import java.io.File; // Import the File class
+import java.io.FileWriter; // Import the FileWriter class
+import java.io.IOException; // Import the IOException class to handle errors
+
 public class Main {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 
-		
-		System.out.println("hello word");
-	
+		//create files for both the customer and transaction datasets
+		try {
+			File customers = new File("./Data/Customers.txt");
+			File transactions = new File("./Data/Transactions.txt");
+			if (customers.createNewFile() && transactions.createNewFile()) {
+				System.out.println("Files created");
+			} else {
+				System.out.println("File already exists.");
+			}
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+
+
+		try {
+			FileWriter myWriter = new FileWriter("./Data/filename.txt", true);
+			BufferedWriter bw = new BufferedWriter(myWriter);
+			bw.write("Spain");
+			bw.newLine();
+			bw.close();
+			System.out.println("Successfully wrote to the file.");
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
 	}
+
 }
